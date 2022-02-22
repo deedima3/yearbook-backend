d6a3820fd0c8f9fa4809b9bb579fee379f288e03
Maheswara Devo <pundadevo21@gmail.com>
2022-02-22 12:09:08 +0800

    add go modules

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/deedima3/yearbook-backend
+
+go 1.17
+
+require (
+	github.com/SIC-Unud/sicgolib v1.0.3 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,1 +1,33 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+)
+
+// Server acts as an object that will listens to http request
+type Server struct {
+	Address string
+	Handler http.Handler
+}
+
+// ListenAndServe listens to http network with given address and handler. This method blocks the execution
+func (s Server) ListenAndServe() {
+	go func() {
+		if err := http.ListenAndServe(s.Address, s.Handler); err != nil {
+			log.Printf("ERROR (starting server): %v\n", err)
+		}
+	}()
+	log.Printf("INFO (server): server started, listening to %s\n", s.Address)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	log.Println("INFO (server): shutting down server")
+}
+
+// ProvideServer acts as a Factory Design Pattern to provide Server struct object in a consistent manner
+func ProvideServer(address string, handler http.Handler) Server {
+	return Server{Address: address, Handler: handler}
+}
