84e1f25a931af5c7e5902ec258e787d162d7459d
Maheswara Devo <pundadevo21@gmail.com>
2022-04-19 14:16:22 +0800

    [#18] change database password

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -10,7 +10,7 @@ import (
 )
 
 func GetDatabase() (*sql.DB) {
-	dataSourceName := "2zlj4cmjec4v:pscale_pw_pSddB2ehFi8I0Bf3plUoeG2NWp2t6HMuPmuyg8BgDAw@tcp(ddzkodr24h6n.ap-southeast-2.psdb.cloud)/yearbook_db?tls=true"
+	dataSourceName := "zh9b94e86tl6:pscale_pw_ftOx0YKNFJKmFaWI-gjNtyReEfUubj-QwYfrLcI_PiE@tcp(ddzkodr24h6n.ap-southeast-2.psdb.cloud)/yearbook_db?tls=true"
 	db, err := sql.Open("mysql", dataSourceName)
 	if err != nil {
 		log.Printf("ERROR GetDatabase sql open connection fatal error: %v\n", err)
