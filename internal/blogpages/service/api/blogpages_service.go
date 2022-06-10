a7a1732f803ccb71d4a1dfdffab35ce0391c5c07
Maheswara Devo <pundadevo21@gmail.com>
2022-06-10 14:15:20 +0800

    [#55]feat: add search blogpages flow

--- internal/blogpages/service/api/blogpages_service.go
+++ internal/blogpages/service/api/blogpages_service.go
@@ -10,4 +10,7 @@ type BlogpagesService interface {
 	ViewUserPages(ctx context.Context, id uint64) (dto.BlogPagesResponse, error)
 	GetAllPages(ctx context.Context) (dto.BlogPagesResponse, error)
 	NewUserPages(ctx context.Context, blogpage dto.RequestNewBlogpage) error
+	SearchUserPages(ctx context.Context, nickname string, nim string) (dto.BlogPagesSearchResponses, error)
+	SearchUserNickname(ctx context.Context, nickname string) (dto.BlogPagesSearchResponses, error)
+	SearchUserNim(ctx context.Context, nim string) (dto.BlogPagesSearchResponses, error)
 }
